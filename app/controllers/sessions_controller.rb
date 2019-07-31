8396bb961729b06c05fca87d1e23349d785a13da
tk0358 <tk0358a@yahoo.co.jp>
2019-08-01 07:11:35 +0900

    Update signin and add authentication methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-  
+
+  end
+
+  def create
+    # user = User.find_by(username: params[:session][:username].downcase)
+    # if user && user.authenticate(params[:session][:password])
+    #   session[:user_id] = user.id
+    #   redirect_to root_path
+    # else
+    #   render 'new'
+    # end
   end
   
 end
